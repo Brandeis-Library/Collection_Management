d7de119f341025c978a8c35b87cbfc77499bd8bf
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-10-18 16:14:26 -0700

    create IronMountain component

---
+++ src/components/IronMountain.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class IronMountain extends Component {
+
+    render () {
+        return <h1>This is the Iron Mountain page.</h1>
+    }
+
+}
