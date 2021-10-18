cf9c388761d908c57047ad088658076f889e7452
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-10-18 16:09:15 -0700

    create BulkCheckin component

---
+++ src/components/BulkCheckin.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class BulkCheckin extends Component {
+
+    render () {
+        return <h1>This is the Bulk Checkin page.</h1>
+    }
+
+}
+
