7a1a19462cf5282cc6c69becd68e7a8f6d081ea7
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-10-20 13:57:28 -0700

    create My404Component that shows when a user requested route is not available.

---
+++ src/components/My404Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Mapping extends Component {
+
+    render () {
+        return (
+        <div style={{backgroundColor: '#00BFFF'}}>
+            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/0efaf032676677.568ed0d61d000.gif" alt="404 Star Wars AT-AT Walker" width="85%" height="85%" />
+        </div>)
+    }
+
+}
+
+
