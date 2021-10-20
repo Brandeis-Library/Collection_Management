3bcff2d8265e150f481253da0c878ed986806f00
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-10-20 13:20:30 -0700

    install react-router-dom the starter Switch routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import { Route, Switch, Redirect} from "react-router-dom";
 
-import Home from './components/Home'
-
+import Home from "./components/Home";
+import Inventory from "./components/Inventory";
+import Mapping from "./components/Mapping";
+import Admin from "./components/Admin";
+import BulkCheckin from "./components/BulkCheckin";
+import IronMountain from "./components/IronMountain";
+import My404Component from "./components/My404Component"
+// testing Pretti
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,8 +26,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-      <Home/>
+      </header> */}
+
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/inventory" component={Inventory} />
+        <Route path="/mapping" component={Mapping} />
+        <Route path="/admin" component={Admin} />
+        <Route path="/BulkCheckin" component={BulkCheckin} />
+        <Route path="/IronMountain" component={IronMountain} />
+        <Route path='/404' component={My404Component} />
+        <Redirect from='*' to='/404' />
+      </Switch>
     </div>
   );
 }
