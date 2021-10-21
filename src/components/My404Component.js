709af38223b54815ba0a573671ea77892448906e
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-10-20 22:23:14 -0700

    put styling into the app including favicon/title, app title, and navbar.

--- src/components/My404Component.js
+++ src/components/My404Component.js
@@ -4,8 +4,8 @@ export default class Mapping extends Component {
 
     render () {
         return (
-        <div style={{backgroundColor: '#00BFFF'}}>
-            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/0efaf032676677.568ed0d61d000.gif" alt="404 Star Wars AT-AT Walker" width="85%" height="85%" />
+        <div style={{backgroundColor: '#00BFFF' }}>
+            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/0efaf032676677.568ed0d61d000.gif" alt="404 Star Wars AT-AT Walker" width="675px" height="585px" style={{borderRadius: '1%'}}/>
         </div>)
     }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,39 @@ import { Link} from "react-router-dom";
 export default  class Navbar extends Component {
 
     render () {
+      const linkStyle = {
+        margin: "1rem",
+        textDecoration: "none",
+        color: 'blue',
+        fontWeight: 'bold',
+      };
+
+      const headerStyle = {
+        position: 'absolute',
+  right: '50px',
+        paddingTop: '5px',
+        //paddingLeft: '225px',
+        textAlign: 'right',
+        color: 'blue',
+        fontSize: '30px',
+      }
     return (
-      <div>
-        <Link to="/">Home </Link>
-        <Link to="/inventory">Inventory </Link>
-        <Link to="/Mapping">Mapping </Link>
+      <div style={{
+        display: 'flex',
+        textAlign: 'left',
+        justifyContent: 'left',
+
+        height: '45px',
+        paddingLeft: '10px',
+        borderBottom: '3px solid rgba(0, 0, 128)',
+      }}>
+        <Link style={linkStyle} to="/">Home</Link>
+        <Link style={linkStyle} to="/inventory">Inventory</Link>
+        <Link style={linkStyle} to="/Mapping">Mapping</Link>
+        <Link style={linkStyle} to="/admin">Admin</Link>
+        <Link style={linkStyle} to="/BulkCheckin">Bulk Checkin</Link>
+        <Link style={linkStyle} to="/IronMountain">Iron Mountain</Link>
+        <div style={headerStyle} >Brandeis Collection Management App</ div>
       </div>
     );
     }
