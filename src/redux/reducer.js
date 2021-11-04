ed3fad28fc13a9e097b01bfc60a8d3291384f1e3
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-11-03 17:28:45 -0700

    add action tupe file and type INCREMENT

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
+import { INCREMENT } from './types';
 
 const inventoryReducer = (state = {inventory:4}, action) => {
     switch (action.type) {
+        case INCREMENT:
+        return {...state, inventory: state.inventory + 1};
         default: return state
     }
 }
