8638323d6cf812e30e27acacdeef3591a353fe18
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-11-15 16:48:59 -0800

    install morgan, dotenv,  and nodemon. Created npm start process in package.json. Add morgan, dotenv, and body parser to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const morgan = require('morgan');
+
+const port = process.env.PORT || 4000
+
+//load env variables
+dotenv.config({ path: './config/config.env' });
+
 const app = express()
-const port = 4000
+
+// middleware
+
+// body parser
+app.use(express.json());
+
+
+// logger
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Collection Management app listening at http://localhost:${port}`)
 })
