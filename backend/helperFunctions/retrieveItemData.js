b880278d3293c53a62f4ece967b8422997fabeef
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-11-22 15:44:54 -0800

    pull most of the datafields out of Alma data object for initial retreival

--- backend/helperFunctions/retrieveItemData.js
+++ backend/helperFunctions/retrieveItemData.js
@@ -1,5 +1,6 @@
 
 const itemRecordObj = {
+    dataObjTotal: {},
     barcode: '',
     title: '',
     mms_id: '',
@@ -7,19 +8,34 @@ const itemRecordObj = {
     itemID: '',
     status: '',
     callNum: '',
-    permLib: '',
+    permLib:  '',
     permLoc: '',
     tempLib: '',
     tempLoc: '',
     string583a: '',
     inventoryDate: '',
     internalNote3: '',
-    dataObj: {},
     link: '',
   };
 
   const retrieveDataItems = (data) => {
+
     itemRecordObj.link = data.link;
+     itemRecordObj.title = data.bib_data.title;
+     itemRecordObj.mms_id = data.bib_data.mms_id;
+     itemRecordObj.holdingID = data.holding_data.holding_id;
+     itemRecordObj.itemID = data.item_data.pid;
+     itemRecordObj.status = data.item_data.base_status.desc;
+     itemRecordObj.callNum = data.holding_data.call_number;
+     itemRecordObj.permLib =  data.item_data.library.desc;
+     itemRecordObj.permLoc = data.item_data.location.desc;
+     itemRecordObj.tempLib =  data.holding_data.temp_library.desc;
+     itemRecordObj.tempLoc = data.holding_data.temp_location.desc;
+     itemRecordObj.string583a = '';
+     itemRecordObj.inventoryDate =  data.item_data.inventory_date || 'None';
+     itemRecordObj.internalNote3 = data.item_data.internal_note_3;
+     itemRecordObj.dataObjTotal = data;
+     itemRecordObj.barcode = data.item_data.barcode;
 
     return itemRecordObj;
   }
