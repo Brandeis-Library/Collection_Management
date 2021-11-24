a573a4178a525e34581a8ec7c9cebab8b7b55db3
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-11-24 14:14:56 -0800

    add fields to actions

--- frontend/src/redux/actions.js
+++ frontend/src/redux/actions.js
@@ -30,11 +30,26 @@ export function sendBarCode(dataObj) {
    return {
       type: SENDBARCODE,
       payload: {
-         "barcode2": dataObj.barcode, 
-         "title":    dataObj.title,
-         "status":   dataObj.status,
-         "callNum":  dataObj.callNum,
-         "permLib":  dataObj.permLib,
+         "barcode2":    dataObj.barcode, 
+         "title":       dataObj.title,
+         "status":      dataObj.status,
+         "callNum":     dataObj.callNum,
+         "permLib":     dataObj.permLib,
+         "dataObjTotal":dataObj.dataObjTotal,
+         "mms_id":      dataObj.mms_id,
+         "holdingID":   dataObj.holdingID,
+         "itemID":      dataObj.itemID,
+         "permLoc":     dataObj.permLoc,
+         "tempLib":     dataObj.tempLib,
+         "tempLoc":     dataObj.tempLoc,
+         "string583a":  dataObj.string583a,
+         "inventoryDate":dataObj.inventoryDate,
+         "internalNote3":dataObj.internalNote3,
+         "link":        dataObj.link,
+         "replacementCost":dataObj.replacementCost,
+         "provenance":  dataObj.provenance,
+         "condition":   dataObj.condition,
+       
 
       
       
