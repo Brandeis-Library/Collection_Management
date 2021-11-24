3e7fe50f165a3e3aa812f6faedbc8789a46df0a2
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-11-24 14:15:15 -0800

    add fields to reducer

--- frontend/src/redux/inventory_reducer.js
+++ frontend/src/redux/inventory_reducer.js
@@ -5,6 +5,23 @@ const initialState = {
     barcode: "", 
     barcode2: "", 
     title: "",
+    dataObjTotal: {},
+    mms_id: '',
+    holdingID: '',
+    itemID: '',
+    status: '',
+    callNum: '',
+    permLib:  '',
+    permLoc: '',
+    tempLib: '',
+    tempLoc: '',
+    string583a: '',
+    inventoryDate: '',
+    internalNote3: '',
+    link: '',
+    replacementCost: '',
+    provenance: '',
+    condition: '',
 }
 
 
@@ -17,7 +34,29 @@ export const inventoryReducer = (state = initialState, action) => {
         case BARCODE:
             return {...state, barcode: action.payload.text.text };
         case SENDBARCODE:
-            return {...state, barcode2: action.payload.barcode2, title: action.payload.title,};
+            return {
+                ...state, 
+                barcode2: action.payload.barcode2, 
+                title: action.payload.title,
+                dataObjTotal: action.payload.dataObjTotal,
+                mms_id: action.payload.mms_id,
+                holdingID: action.payload.holdingID,
+                itemID: action.payload.itemID,
+                status: action.payload.status,
+                callNum: action.payload.callNum,
+                permLib: action.payload.permLib,
+                permLoc: action.payload.permLoc,
+                tempLib: action.payload.tempLib,
+                tempLoc: action.payload.tempLoc,
+                string583a: action.payload.string583a,
+                inventoryDate: action.payload.inventoryDate,
+                internalNote3: action.payload.internalNote3,
+                link: action.payload.link,
+                replacementCost: action.payload.replacementCost,
+                provenance: action.payload.provenance,
+                condition: action.payload.condition,
+            
+            };
         default: return state
     }
 }
