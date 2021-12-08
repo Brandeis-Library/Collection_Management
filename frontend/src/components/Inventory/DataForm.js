301cdeae8e35a9a48e5a4aae965cc885b58c66e0
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-12-08 14:58:21 -0800

    add fields with local state and change even handler.

--- frontend/src/components/Inventory/DataForm.js
+++ frontend/src/components/Inventory/DataForm.js
@@ -2,14 +2,74 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class DataFormContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      note3: "",
+      price: "",
+      provenance: "",
+      condition: "",
+    };
+  }
+
+ handleInputChange = (event) => {
+    //const target = event.target;
+    //const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = event.target.name;
+    const value = event.target.value
+//console.log("++++++++++ Target----", event.target, "name", event.target.name, "value", event.target.value);
+    this.setState({
+      [name]: value
+    });
+  }
   render() {
     return (
       <React.Fragment>
-        <h5>This is the DataForm page.</h5>
-        <p>DataFormContainer</p>
-        <p>{this.props.inventory}</p>
-        <p>#1 {this.props.barcode}</p>
-        <p>#2 {this.props.barcode2}</p>
+        <h5>Update Item Information</h5>
+        <br/>
+        <form>
+          <label>
+            Note:{' '}
+            <input
+              name="note3"
+              type="text"
+              value={this.state.note3}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <br />
+          <label>
+          Price:{' '}
+            <input
+              name="price"
+              type="text"
+              value={this.state.price}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <br />
+          <label>
+          Provenance: {' '}
+          <select name="provenance" value={this.state.value} onChange={this.handleInputChange} type="text">
+            <option value="">None</option>
+            <option value="JCR">Jewish Cultural Reconstruction</option>
+          
+          </select>
+        </label>
+        <br />
+          <br />
+          <label>
+         Condition: {' '}
+          <select name="condition" value={this.state.value} onChange={this.handleInputChange} type="text">
+            <option value="">None</option>
+            <option value="BRITTLE">Brittle</option>
+            <option value="DAMAGED">Damaged</option>
+            <option value="DETERIORATING">Deteriorating</option>
+            <option value="FRAGILE">Fragile</option>
+          </select>
+        </label>
+      </form>
+    
       </React.Fragment>
     );
   }
