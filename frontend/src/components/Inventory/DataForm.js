a6186c9380161d3a2c383579cd2dd01e1c8e165e
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2021-12-09 16:08:12 -0800

    add code to take data from submitted form to get it be presented to Redux

--- frontend/src/components/Inventory/DataForm.js
+++ frontend/src/components/Inventory/DataForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { updateItem } from "../../redux/actions";
 
 class DataFormContainer extends Component {
   constructor(props) {
@@ -12,64 +13,83 @@ class DataFormContainer extends Component {
     };
   }
 
- handleInputChange = (event) => {
+  handleInputChange = (event) => {
     //const target = event.target;
     //const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value
-//console.log("++++++++++ Target----", event.target, "name", event.target.name, "value", event.target.value);
+    const value = event.target.value;
+    //console.log("++++++++++ Target----", event.target, "name", event.target.name, "value", event.target.value);
     this.setState({
-      [name]: value
+      [name]: value,
     });
-  }
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.updateItem({
+      internalNote3: this.state.note3,
+      replacementCost: this.state.price,
+      provenance: this.state.provenance,
+      condition: this.state.condition,
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
         <h5>Update Item Information</h5>
-        <br/>
+        <br />
         <form>
           <label>
-            Note:{' '}
+            Note:{" "}
             <input
               name="note3"
               type="text"
               value={this.state.note3}
-              onChange={this.handleInputChange} />
+              onChange={this.handleInputChange}
+            />
           </label>
           <br />
           <br />
           <label>
-          Price:{' '}
+            Price:{" "}
             <input
               name="price"
               type="text"
               value={this.state.price}
-              onChange={this.handleInputChange} />
+              onChange={this.handleInputChange}
+            />
           </label>
           <br />
           <br />
           <label>
-          Provenance: {' '}
-          <select name="provenance" value={this.state.value} onChange={this.handleInputChange} type="text">
-            <option value="">None</option>
-            <option value="JCR">Jewish Cultural Reconstruction</option>
-          
-          </select>
-        </label>
-        <br />
+            Provenance:{" "}
+            <select
+              name="provenance"
+              value={this.state.value}
+              onChange={this.handleInputChange}
+              type="text">
+              <option value="">None</option>
+              <option value="JCR">Jewish Cultural Reconstruction</option>
+            </select>
+          </label>
+          <br />
           <br />
           <label>
-         Condition: {' '}
-          <select name="condition" value={this.state.value} onChange={this.handleInputChange} type="text">
-            <option value="">None</option>
-            <option value="BRITTLE">Brittle</option>
-            <option value="DAMAGED">Damaged</option>
-            <option value="DETERIORATING">Deteriorating</option>
-            <option value="FRAGILE">Fragile</option>
-          </select>
-        </label>
-      </form>
-    
+            Condition:{" "}
+            <select
+              name="condition"
+              value={this.state.value}
+              onChange={this.handleInputChange}
+              type="text">
+              <option value="">None</option>
+              <option value="BRITTLE">Brittle</option>
+              <option value="DAMAGED">Damaged</option>
+              <option value="DETERIORATING">Deteriorating</option>
+              <option value="FRAGILE">Fragile</option>
+            </select>
+          </label>
+        </form>
       </React.Fragment>
     );
   }
@@ -84,4 +104,14 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, null)(DataFormContainer);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // barcode: (text) => dispatch(barcode(text)),
+    // sendBarcodeToBackend: (text) => dispatch(sendBarcodeToBackend(text)),
+    updateItem: (obj) => {
+      dispatch(updateItem(obj));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DataFormContainer);
