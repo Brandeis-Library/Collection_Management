0657f25ecbb6da71856d64ad1b4577ce225dd4c6
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-01-05 17:30:48 -0800

    create UseStationStorage

---
+++ frontend/src/components/Inventory/UseSessionStorage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class UseSessionStorage extends Component {
+  render() {
+    return (
+      <div>
+        <React.Fragment>UseSessionStorage Component</React.Fragment> <br />
+      </div>
+    );
+  }
+}
