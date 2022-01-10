d16d32209a54650ff958980cf91004dd1f225d9a
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-01-10 10:52:02 -0800

    add code to have local storage component show live changes to the current call # for checking.

--- frontend/src/components/Inventory/UseLocalStorage.js
+++ frontend/src/components/Inventory/UseLocalStorage.js
@@ -3,19 +3,24 @@ import React, { Component } from "react";
 export default class UseSLocaltorage extends Component {
   constructor(props) {
     super(props);
-    this.state = { firstCallNum: "" };
+    this.state = { firstCallNum: "", localStorageState: "" };
   }
 
   handleChange = (event) => {
     this.setState({ firstCallNum: event.target.value });
   };
 
+  componentDidMount() {
+    this.setState({ localStorageState: localStorage.getItem("CallNumforTest") });
+  }
+
   handleSubmit = (event) => {
     event.preventDefault();
 
     if (this.state.firstCallNum) {
       localStorage.setItem("CallNumforTest", this.state.firstCallNum);
-      //this.props.firstCallNum({ text: this.state.barcode });
+
+      this.setState({ localStorageState: localStorage.getItem("CallNumforTest") });
       this.setState({ firstCallNum: "" });
     } else {
       alert("Please enter a LC Call Number.");
@@ -26,7 +31,8 @@ export default class UseSLocaltorage extends Component {
     console.log("firstCallNum", this.state.firstCallNum);
     return (
       <div>
-        <React.Fragment>UseLocalStorage Component</React.Fragment> <br />
+        {/* <React.Fragment>UseLocalStorage Component</React.Fragment> <br /> */}
+        Current LC Call #: {this.state.localStorageState} <br />
         <form onSubmit={this.handleSubmit}>
           <label>
             Enter First Call #:
@@ -42,8 +48,7 @@ export default class UseSLocaltorage extends Component {
           <input type="submit" value="Submit" />
         </form>
         <br />
-        Current LC Call # to check against local state: {this.state.firstCallNum} <br />
-        Current LC Call # to check against local storage {localStorage.getItem("CallNumforTest")}
+        {/* Current LC Call # to check against local storage {localStorage.getItem("CallNumforTest")} */}
       </div>
     );
   }
