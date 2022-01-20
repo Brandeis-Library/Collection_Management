4f84fa70e03f19c006814baa59cf147cf03d7c67
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-01-19 18:00:53 -0800

    implement basic hooks to get started.

--- frontend/src/components/Inventory/ReduxForm.js
+++ frontend/src/components/Inventory/ReduxForm.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-const ReduxForm = () => (
-  <div>
-    <h5>Update Item Information</h5>
-  </div>
-);
+const ReduxForm = () => {
+  const inventoryData = useSelector((state) => state.inventory);
+
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h5>Update Item Information</h5>
+    </div>
+  );
+};
 
 export default ReduxForm;
