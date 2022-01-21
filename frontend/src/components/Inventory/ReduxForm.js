a8923aac10ea6b04755af5972ad2435262ec511d
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-01-21 13:28:09 -0800

    add items to complete as a todo list in onSubmit in ReduxForm.

--- frontend/src/components/Inventory/ReduxForm.js
+++ frontend/src/components/Inventory/ReduxForm.js
@@ -9,13 +9,27 @@ const ReduxForm = () => {
     setPrice(inventoryData.item_data.replacement_cost);
   }, [inventoryData]);
 
+  const onSubmit = (event) => {
+    event.prevenDefault();
+    // gather local state
+    // attach to inventoryData
+    // bring in the dispatch process you want
+    // declate dispatch
+    // submit data to dispatch
+    // set up action type
+    // set up action
+    // thunk to back end
+    // when data comes back sent to reducer
+    // set up reducer
+  };
+
   const dispatch = useDispatch();
   return (
     <div>
       {/* {JSON.stringify(inventoryData)} */}
       {/* {console.log(inventoryData.item_data.replacement_cost)} */}
       <h5>Update Item Information</h5>
-      <form>
+      <form onSubmit={onSubmit}>
         <label>
           Price:
           <input type="text" value={price} onChange={(event) => setPrice(event.target.value)} />
