1b98e80d959eabe0a33e2246bc16d4b03a8d7413
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-02-11 15:58:40 -0800

    cleaned out console.logs, fixed formatting, and added notes to the invetory.js routes file.

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -11,13 +10,12 @@ const lc = require("lc_call_number_compare");
 const retrieveDataItems = require("../helperFunctions/retrieveItemData");
 const replacementCost = require("../helperFunctions/replacmentCost");
 
-// route to retrieve record by barcode
+// route to retrieve record by barcode. Initial inventory data retrieval.
 router.post("/", async function (req, res, next) {
   try {
     console.log("req.body.barcode.text)", req.body.barcode.text);
     const barcode = req.body.barcode.text;
     //item retrieve query to Alma backend. API URL, Item Barcode, and APIKEY
-
     const { data } = await axios.get(
       process.env.EXLIBRIS_API_ROOT +
         process.env.EXLIBRIS_API_PATH +
@@ -26,28 +24,20 @@ router.post("/", async function (req, res, next) {
         process.env.EXLIBRIS_API_BIB_GET_KEY +
         "&expand=p_avail",
     );
-
-    //console.log("physical_condition ======== ", data.item_data.physical_condition);
+    //send data to a helper function to format the data for the frontend data store.
     const dataObj = retrieveDataItems(data);
-    //console.log("dataObj", dataObj);
     res.status(200).send({ dataObj: dataObj });
   } catch (error) {
-    console.log("catch for overall in inventory root post method.");
-    //console.log("retreiveItemErrorAPI Error:   ", error);
-    console.log("retreiveItemErrorAPI Error:   ", error.message);
+    console.error("retreiveItemErrorAPI Error:   ", error.message);
+    //sends the error message to the on board express error handler.
     next({ status: false, message: error.message });
   }
 });
 
+// route for sending the updating data from the item update frontend form.
 router.put("/itemform", async function (req, res, next) {
   try {
-    // console.log("inside iventory itemform route -----------------");
-    console.log("req.body", req.body);
-    // console.log("itemform dataObj from the frontend ------------- ", req.body.obj.internalNote3);
     const dataObj = req.body.obj;
-
-    console.log("updated dataObj -------- ", dataObj);
-
     let { data } = await axios.put(
       process.env.EXLIBRIS_API_ROOT +
         "/almaws/v1/bibs/" +
@@ -60,15 +50,14 @@ router.put("/itemform", async function (req, res, next) {
         process.env.EXLIBRIS_API_BIB_UPDATE_KEY,
       dataObj,
     );
-    console.log("data from update on itemform route from alma/axios", data);
     res.json(data);
   } catch (error) {
-    console.log("updateItemErrorAPI Error:   ", error.message);
+    console.error("updateItemErrorAPI Error:   ", error.message);
     next({ status: false, message: error.message });
   }
 });
 
-// Route to retreive 538a field data.
+// Route to retreive 538a field data currently being used for committed to retain.
 router.put("/538Text", async function (req, res, next) {
   try {
     const mmsId = req.body.obj.bib_data.mms_id;
@@ -84,14 +73,13 @@ router.put("/538Text", async function (req, res, next) {
         process.env.EXLIBRIS_API_BIB_GET_KEY,
     );
 
-    //pulling out the XML hodings data and searching to see if 'committed to retain' is in the text. If the text is found returning the text to the front end to show.
+    // Pulling out the XML hodings data and pulls out any text info in the 538a field.
+    // The system no longer just searches for committed to retain only.
     let string583a = "";
     let document = data.anies[0];
     document = document.toString();
-    //console.log("document", document);
     let xmlParsedDoc = await new dom().parseFromString(document);
     string583a = xpath.select("//datafield[@tag=583]/subfield", xmlParsedDoc);
-    //console.log("string583a----------   ", string583a);
     if (string583a.length === 0) {
       string583a = "----";
     } else {
@@ -99,10 +87,10 @@ router.put("/538Text", async function (req, res, next) {
       string583a = string583a.replace(/<subfield(.*?)>/g, "");
       string583a = string583a.replace(/<\/subfield>/g, "");
     }
-
+    // Just sends back the string text. Not in obj form.
     res.send(string583a);
   } catch (error) {
-    console.log("updateItemErrorAPI Error:   ", error.message);
+    console.error("updateItemErrorAPI Error:   ", error.message);
     next({ status: false, message: error.message });
   }
 });
@@ -110,21 +98,15 @@ router.put("/538Text", async function (req, res, next) {
 // route to do the checking of order of call numbers for order of books on the shelves.
 router.put("/callNumCheck", async function (req, res, next) {
   try {
-    const dataObj = req.body;
-
-    console.log("dataObj from callNumbCheck ---------", dataObj);
     //let firstCal cd  Does not work, Needs to be sent and changed on the front end.
     const x = req.body.x;
     const y = req.body.y;
-    console.log("x & y from callNumbCheck ---------", x, y);
-
-    let result = await lc.lte(x, y); /* true */
-
-    console.log("result +++++++++++++++++", result);
-
+    // uses npm package ld to see if the LC call numbers are in the proper order.
+    let result = await lc.lte(x, y);
+    //status is true or false
     res.json({ status: result });
   } catch (error) {
-    console.log("uppdate callNumCheck API Error:   ", error.message);
+    console.error("uppdate callNumCheck API Error:   ", error.message);
     next({ status: false, message: error.message });
   }
 });
@@ -132,11 +114,10 @@ router.put("/callNumCheck", async function (req, res, next) {
 // Route to do autoupdate of Inventory Date and Check Replacement Cost
 router.put("/", async function (req, res) {
   try {
-    //console.log("Inventory main put route - req.body ++++++ )", req.body.respDataObj);
     const dataObj = req.body.respDataObj.dataObjTotal;
-    //console.log("dataObj item-data", dataObj.item_data);
 
     let cost = dataObj.item_data.replacement_cost;
+    // use helper function to check for replacement cost.
     cost = await replacementCost(cost);
     dataObj.item_data.replacement_cost = cost;
 
@@ -155,22 +136,20 @@ router.put("/", async function (req, res) {
         process.env.EXLIBRIS_API_BIB_UPDATE_KEY,
       dataObj,
     );
-    //console.log("data --------------------- ", data);
+    // sends updated data obj received from Alma back to the front end.
     res.json(data);
   } catch (error) {
-    console.log("updateItemErrorAPI Error:   ", error.message);
+    console.error("updateItemErrorAPI Error:   ", error.message);
     next({ status: false, message: error.message });
   }
 });
 
-// define the home page route
-// generic get route to test avaialablity
+// generic get route @ root to test avaialablity
 router.get("/", function (req, res, next) {
   try {
-    //console.log("req.body.barcode)",req.body.barcode)
     res.status(200).send("Inventory home page");
   } catch (error) {
-    console.log("inventory Root Get Error:   ", error.message);
+    console.error("inventory Root Get Error:   ", error.message);
     next({ status: false, message: error.message });
   }
 });
