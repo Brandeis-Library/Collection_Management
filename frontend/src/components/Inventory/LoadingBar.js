ba55148e4c1456205cd667a2a3a02f3c197e668b
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-02-11 17:19:11 -0800

    clean up and add comments to LoadingBar.js

--- frontend/src/components/Inventory/LoadingBar.js
+++ frontend/src/components/Inventory/LoadingBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import LoadingBar from "react-redux-loading-bar";
 
+// Component for loading/running the loading bar package.
 class Header extends Component {
   render() {
     return (
