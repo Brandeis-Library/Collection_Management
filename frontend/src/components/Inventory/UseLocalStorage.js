da83f0be82c52638f535badb6c73ee85fb042a4d
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-02-11 17:52:02 -0800

    add notes. Modify message to make fuctional for blank call number being entered. Also fixed to clear message when clear is used and when a call # is entered.

--- frontend/src/components/Inventory/UseLocalStorage.js
+++ frontend/src/components/Inventory/UseLocalStorage.js
@@ -3,41 +3,50 @@ import { connect } from "react-redux";
 import { updateMessage } from "../../redux/actions";
 import { Button } from "react-bootstrap";
 
+// componet to manage the call number # to be used for the sorting algorithm.
 class UseLocalStorageContainer extends Component {
   constructor(props) {
     super(props);
     this.state = { firstCallNum: "" };
   }
 
+  // processes data that is typed into Call # text box into local state
   handleChange = (event) => {
     this.setState({ firstCallNum: event.target.value });
   };
 
+  // clears curent call number in local storage
   handleClear = (event) => {
     event.preventDefault();
     localStorage.removeItem("CallNumforTest");
     const obj = {
-      status: this.props.status,
-      message: this.props.message,
+      status: "None",
+      message: "",
       localStorageCallNum: "",
     };
     this.props.updateMessage({ obj });
   };
 
+  //
   handleSubmit = (event) => {
     event.preventDefault();
 
     if (this.state.firstCallNum) {
       localStorage.setItem("CallNumforTest", this.state.firstCallNum);
       const obj = {
-        status: this.props.status,
-        message: this.props.message,
+        status: "None",
+        message: "",
         localStorageCallNum: this.state.firstCallNum,
       };
       this.props.updateMessage({ obj });
       this.setState({ firstCallNum: "" });
     } else {
-      alert("Please enter a LC Call Number.");
+      const obj = {
+        status: false,
+        message: "Please enter a LC Call Number.",
+        localStorageCallNum: localStorage.getItem("CallNumforTest"),
+      };
+      this.props.updateMessage({ obj });
     }
   };
 
