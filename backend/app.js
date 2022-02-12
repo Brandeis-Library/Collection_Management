510f563d30e60f64eb2c1ae526951bcbc30e5aa2
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-02-11 16:05:06 -0800

    clean up and add more notes to the app.js main backend file.

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,38 @@
-const express = require('express')
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const cors = require('cors')
-
-const port = process.env.PORT || 4000
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const port = process.env.PORT || 4000;
 
 //load env variables
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
-const inventoryRouter = require('./routes/inventory')
+// import routes
+const inventoryRouter = require("./routes/inventory");
 
-const app = express()
+// instantiate express app
+const app = express();
 
-// middleware
+// Middleware
 
 // body parser
 app.use(express.json());
 
-
 // logger
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 // cors
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Collection Mangement application backend!')
-})
+// home route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Collection Mangement application backend!");
+});
 
-//create routes
-app.use('/api/v1/inventory', inventoryRouter);
+// create routes
+app.use("/api/v1/inventory", inventoryRouter);
 
+// server
 app.listen(port, () => {
-  console.log(`Collection Management app listening at http://localhost:${port}`)
-})
+  console.log(`Collection Management app listening at http://localhost:${port}`);
+});
