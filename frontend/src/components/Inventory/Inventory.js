6e2126a79a493bf02a83d2f89e26fe821e01b3e3
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-02-11 17:18:06 -0800

    clean up and add comments to Inventory.js

--- frontend/src/components/Inventory/Inventory.js
+++ frontend/src/components/Inventory/Inventory.js
@@ -2,14 +2,12 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import ReceiveBarcode from "./ReceiveBarcode";
 import TilesHolder from "./TilesHolder";
-import Header from "./LoadingBar";
 
+// The top level container for the Inventory Section of the application.
 class InventoryContainer extends Component {
   render() {
     return (
       <React.Fragment>
-        {/* <h1>This is the Inventory page.</h1> */}
-        {/* <Header /> */}
         <ReceiveBarcode />
         <TilesHolder />
       </React.Fragment>
@@ -19,7 +17,6 @@ class InventoryContainer extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    //inventory: state.inventory.inventory,
     barcode: state.inventory.barcode,
     barcode2: state.inventory.barcode2,
   };
