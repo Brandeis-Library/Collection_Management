4c99574242f3833a0b183e1031b63ee410bb3d3c
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-02-14 15:11:32 -0800

    remove unneeded code from store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-//import { loadingBarMiddleware } from "react-redux-loading-bar";
 import rootReducer from "./reducer";
 
 const store = createStore(
