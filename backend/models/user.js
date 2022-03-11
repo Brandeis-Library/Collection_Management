acc49670fa2e21a768fdc1f002304f2174635a20
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-03-10 18:15:12 -0800

    add user model/schema

---
+++ backend/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name."],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password."],
+    minlength: 6,
+    select: false, // Will not send this field when User model requested via API
+  },
+  role: {
+    type: String,
+    required: [true, "Please add a name."],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
