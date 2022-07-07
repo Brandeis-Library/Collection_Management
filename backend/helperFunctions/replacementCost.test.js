234e656e60d37f3fb2e8e1953e2669aad8163b4e
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-07-06 18:27:38 -0700

    add tests 3-5 for replacement cost

--- backend/helperFunctions/replacementCost.test.js
+++ backend/helperFunctions/replacementCost.test.js
@@ -15,4 +15,30 @@ describe("replacementCost()", () => {
 
     expect(result).toBe("100");
   });
+
+  it("should return the price if the price entered if more than 100", () => {
+    const price = "451";
+    const result = replacementCost(price);
+
+    expect(result).toBe("451");
+  });
+
+  it("should return 100 if the price entered if less than 0", () => {
+    const price = "-1";
+    const result = replacementCost(price);
+
+    expect(result).toBe("100");
+  });
+
+  it("should still function properly if a number not a string is entered", () => {
+    const price = 45;
+    const price2 = 145;
+    const result = replacementCost(price);
+    const result2 = replacementCost(price2);
+
+    expect(result).toBe("100");
+    expect(result2).toBe("145");
+  });
 });
+
+// will accept a number price
