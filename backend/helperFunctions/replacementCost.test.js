16dbe2630871adf03d519e6a0fa8bb828a3d4745
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-07-06 18:12:35 -0700

    add 2nd test to replacement cost and repaired the first test

--- backend/helperFunctions/replacementCost.test.js
+++ backend/helperFunctions/replacementCost.test.js
@@ -1,12 +1,18 @@
 import { it, expect, describe } from "vitest";
 
-import { replacementCost } from "./replacementCost";
+import replacementCost from "./replacementCost.js";
 
 describe("replacementCost()", () => {
-  it("should return a price of 100 if no price is intered"),
-    () => {
-      const result = replacementCost(price);
+  it("should return a price of 100 if no price is entered", () => {
+    const result = replacementCost();
 
-      expect(result).toBe(100);
-    };
+    expect(result).toBe("100");
+  });
+
+  it("should return 100 if the price entered if less than 100", () => {
+    const price = "45";
+    const result = replacementCost(price);
+
+    expect(result).toBe("100");
+  });
 });
