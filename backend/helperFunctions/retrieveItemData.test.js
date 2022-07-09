bbed3b648422f85b408ce43f2d39a4ddd928bb05
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-07-08 19:34:09 -0700

    add a 2nd test toretreiveitemdata

--- backend/helperFunctions/retrieveItemData.test.js
+++ backend/helperFunctions/retrieveItemData.test.js
@@ -5,23 +5,29 @@ import retrieveDataItems from "./retrieveItemData";
 import { item } from "./itemTestData.js";
 
 describe("retrieveDataItems()", () => {
-  it("should still function properly if a number not a string is entered", () => {
-    expect("145").toBe("145");
-  });
+ 
 
-  it("should return all the necessary data when an item if found and not in a temp location", () => {
-    console.log(item);
+  it("should return should return the barcode broken out from the original data obj", () => {
+    //console.log("-------------", item);
     const item1 = retrieveDataItems(item.data);
-
+    //console.table(item1);
     // const mmsId = user1.dataObjTotal.bib_data.mms_id;
     // const holdId = user1.dataObjTotal.holding_data.holding_id;
     // const tempLocBool = user1.dataObjTotal.holding_data.in_temp_location;
 
-    expect(1).toEqual(1);
+    expect(item1.barcode).toEqual(item.data.item_data.barcode);
   });
 
+  it("should return all the necessary data when an item if found and not in a temp location", () => {
+    const item1 = retrieveDataItems(item.data);
+    expect(item1.tempLib).toBe("-----");
+  });
+
+
   // it("should return all the necessary data when an item if found and in a temp location", () => {
-  //   console.log(user.dataObjTotal);
+  //   item.data.holding_data.in_temp_location = true;
+
+  //   const item1 = retrieveDataItems(item.data);
   //   expect(1).toEqual(1);
   // });
 });
