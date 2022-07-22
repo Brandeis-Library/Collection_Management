555665ef6e84b62f787488de330289b7e18e61c8
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2022-07-21 21:00:18 -0700

    attempting to create tests for string583a.js

--- backend/helperFunctions/string583a.test.js
+++ backend/helperFunctions/string583a.test.js
@@ -1,1 +1,39 @@
-import { it, expect, describe, beforeEach } from "vitest";
+const fs = require('fs/promises');
+import { it, expect, describe, beforeEach, vi } from "vitest";
+vi.mock('fs');
+
+import retrieve538aString from './string583a';
+//import { } from './583aTestDataNone.xml';
+
+describe("retrieve538aString()", () => {
+
+    it("returns ---- when there is no 538a field.", async () => {
+
+        // await fs.readFileSync('./583aTestDataNone.xml', { encoding: 'utf-8' }, (error) => {
+        //     if (error) throw error
+
+        // });
+        let xmlData;
+        console.log(__dirname);
+        await fs.readFile(__dirname + '/583aTestDataNone.xml',
+            //{ encoding: 'utf16le', flag: 'r' },
+            'utf16le',
+            async function (err, data) {
+                // if (err) {
+                //     console.log(err);
+                //     return;
+                // }
+
+
+                xmlData = await data;
+                console.log("data inside if----------------------", await data);
+            });
+
+
+        console.log("xmldata-------------", xmlData);
+        expect(xmlData).toBeTypeOf('string');
+    });
+
+
+    //it("should fail")
+});
