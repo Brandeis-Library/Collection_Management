a177bd22b01bf8bead5417d0343ee427e2a4b4f2
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2023-07-24 18:05:18 -0700

    ad mongodb package connected mongodb to database.

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,31 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+
 const morgan = require("morgan");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
 
 //load env variables
 dotenv.config({ path: "./config/config.env" });
+const mongoString = process.env.MONGO_DATABASE_URL;
 
 // import routes
 const inventoryRouter = require("./routes/inventory");
+// connectMongoDB
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+// if MongoDB has an error connecting
+database.on('error', (error) => {
+  console.log(error);
+});
+
+// if MongoDB connects successfully.
+database.once('connected', () => {
+  console.log('Database Connected');
+});
 
 // instantiate express app
 const app = express();
