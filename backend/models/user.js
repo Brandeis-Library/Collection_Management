1def32e51a78ed0f9db9065ac70814ab7830988e
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2023-07-27 14:52:07 -0700

    modified userModel and import into authentication.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,11 +11,12 @@ const userSchema = new Schema({
     type: String,
     required: [true, "Please add a password."],
     minlength: 6,
+    maxlength: 25,
     select: false, // Will not send this field when User model requested via API
   },
   role: {
     type: String,
-    required: [true, "Please add a name."],
+    required: [true, "Please add a role."],
   },
   createdAt: {
     type: Date,
--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const userModel = require('../models/user.js');
 const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
