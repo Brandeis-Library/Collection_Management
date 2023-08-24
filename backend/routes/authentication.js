53bfd18b127d6bd904684dae2a5980c10a42310b
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2023-08-23 17:56:54 -0700

    deleted data no longer needed in the route file

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,89 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const userModel = require('../models/user.js');
-const dotenv = require("dotenv");
-dotenv.config();
+//const userModel = require('../models/user.js');
+//const dotenv = require("dotenv");
+//dotenv.config();
+const jwt = require('jsonwebtoken');
 const passport = require('passport');
 
-// //  passport routes
-// app.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
-//     // Generate and send a JWT token
-//     // ...
-// });
 
-// app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
-//     res.json({ message: 'This is a protected route.' });
-// });
 
 // Route default method
 router.get('/', (req, res) => {
     res.send("Authentation home route.");
 });
 
-//Post Method
-router.post('/post', async (req, res) => {
-    const data = new userModel({
-        name: req.body.name,
-        password: req.body.password,
-        role: req.body.role
-    });
-    try {
-        const dataToSave = await data.save();
-        console.log("dataToSave---------", dataToSave);
-        res.status(201).send(dataToSave);
-    } catch (err) {
-        console.error(err);
-        res.status(400).json({ message: err.message });
-    }
+/* POST login. */
+router.post('/login', function (req, res, next) {
+    console.log("login req.body----------", req.body);
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+        if (err || !user) {
+            return res.status(400).json({
+                message: 'Something is not right',
+                user: user
+            });
+        }
+        req.login(user, { session: false }, (err) => {
+            if (err) {
+                res.send(err);
+            }
+            // generate a signed son web token with the contents of user object and return it in the response
+            const token = jwt.sign(user, 'your_jwt_secret');
+            return res.json({ user, token });
+        });
+    })(req, res);
 });
 
-//Get all Method
-router.get('/getAll', async (req, res) => {
-    try {
-        const data = await userModel.find();
-        res.status(200).json({ data });
-    } catch (err) {
-        console.error(err);
-        res.status(400).json({ message: err.message });
-    }
-});
-
-//Get by ID Method
-router.get('/getOne/:id', async (req, res) => {
-    try {
-
-        const data = await userModel.findById(req.params.id);
-        res.status(200).json(data);
-    } catch (err) {
-        console.error(err);
-        res.status(400).json({ message: err.message });
-    }
-});
-
-//Update by ID Method
-router.patch('/update/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
-        const result = await userModel.findByIdAndUpdate(id, updatedData, options);
-        res.status(200).send(result);
-    } catch (err) {
-        console.error(err);
-        res.status(400).json({ message: err.message });
-    }
-});
-
-//Delete by ID Method
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await userModel.findByIdAndDelete(id);
-        res.status(200).send(`User with id ${id} has been deleted. \n\n ${data}`);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-
-});
 
 module.exports = router;
