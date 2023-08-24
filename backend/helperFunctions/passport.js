4fadf34a8f7fcc7edb181719b03accdd42671b9b
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2023-08-23 19:33:57 -0700

    fixed userModel connection in passport

--- backend/helperFunctions/passport.js
+++ backend/helperFunctions/passport.js
@@ -1,30 +1,51 @@
 // security info
+const userModel = require('../models/user.js');
 const passport = require('passport');
 const passportJWT = require("passport-jwt");
+const ExtractJWT = passportJWT.ExtractJwt;
 const LocalStrategy = require('passport-local').Strategy;
 const JWTStrategy = passportJWT.Strategy;
 
 
 // Configure local strategy
 passport.use(new LocalStrategy((name, password, done) => {
+    console.log("name, password", name, password);
     usernameField: 'name';
     passwordField: 'password';
-}, async (name, password, callback) => {
+}, function (name, password, cb) {
+    console.log("name, password", name, password);
+    //Assume there is a DB module pproviding a global UserModel
+    return userModel.find({ name, password })
+        .then(user => {
+            if (!user) {
+                return cb(null, false, { message: 'Incorrect email or password.' });
+            }
+
+            return cb(null, user, {
+                message: 'Logged In Successfully'
+            });
+        })
+        .catch(err => {
+            return cb(err);
+        });
+}
 
 
-    try {
+));
 
-        const user = await userModel.find(name, password);
-        console.log("user------------", user);
-        if (!user) {
-            return callback(null, false, { message: 'Incorrect email or password.' });
-        }
-        return callback(null, user, { message: 'Logged In Successfully' });
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+},
+    function (jwtPayload, cb) {
 
-
-    } catch (err) {
-        console.error(err);
-        res.status(400).json({ message: err.message });
+        //find the user in db if needed
+        return UserModel.findOneById(jwtPayload.id)
+            .then(user => {
+                return cb(null, user);
+            })
+            .catch(err => {
+                return cb(err);
+            });
     }
-
-}));
+));
