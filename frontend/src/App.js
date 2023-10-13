5622365d7b1269fe15943a1654d8dc9f59f0f1fe
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2023-10-13 14:26:52 -0700

    update App.js to implement route security.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import store from "./redux/store";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+import Root from "./components/Root";
 import Inventory from "./components/Inventory/Inventory";
 import Mapping from "./components/Mapping";
 import Admin from "./components/Admin";
@@ -10,15 +11,25 @@ import IronMountain from "./components/IronMountain";
 import My404Component from "./components/My404Component";
 
 function App() {
+
+  const AuthRoute = (props) => {
+    if (!store.getState().account.loggedIn) {
+      return <Redirect to={{ pathname: '/' }} />;
+    } else {
+      const { component, path } = props;
+      return <Route path={path} component={component} />;
+    }
+  };
+
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/inventory" component={Inventory} />
-        <Route path="/mapping" component={Mapping} />
-        <Route path="/admin" component={Admin} />
-        <Route path="/BulkCheckin" component={BulkCheckin} />
+        <Route path="/" component={Root} exact />
+        <AuthRoute path="/inventory" component={Inventory} />
+        <AuthRoute path="/mapping" component={Mapping} />
+        <AuthRoute path="/admin" component={Admin} />
+        <AuthRoute path="/BulkCheckin" component={BulkCheckin} />
         <Route path="/IronMountain" component={IronMountain} />
         <Route path="/404" component={My404Component} />
         <Redirect from="*" to="/404" />
