94be1472b74afc223e0964b6e4d78455880d03f1
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2023-10-20 20:42:37 -0700

    add code to catch blank fields being entered and throw and error.

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import MessageContainer from "./Inventory//Message";
 import { Button, FormGroup, FormControl } from 'react-bootstrap';
-import { validateUser as login } from '../redux/actions';
+import { validateUser as login, updateMessage } from '../redux/actions';
 
 
 class AuthForm extends Component {
@@ -27,10 +28,31 @@ class AuthForm extends Component {
     //     this.props.signup({ username, password });
     // };
 
-    login = () => {
-        const { username, password } = this.state;
-        this.setState({ buttonClicked: true });
-        this.props.login({ username, password });
+    login = (event) => {
+        event.preventDefault();
+        try {
+            const { username, password } = this.state;
+            if (!password || !username) {
+                const obj = {
+                    status: false, message: "username and password must be entered."
+                };
+                this.props.updateMessage({ obj });
+            }
+            //const { username, password } = this.state;
+            else {
+                this.setState({ buttonClicked: true });
+                this.props.login({ username, password });
+            }
+        } catch (error) {
+            console.error("error in login in AuthFormjs", error);
+            const obj = {
+                status: false,
+                message: error.message,
+                localStorageCallNum: localStorage.getItem("CallNumforTest"),
+            };
+            this.props.updateMessage({ obj });
+        }
+
     };
 
     // get Error() {
@@ -46,7 +68,7 @@ class AuthForm extends Component {
 
             <div>
                 <h2> Sign In to use the application.</h2>
-
+                <MessageContainer />
                 <form>
                     <FormGroup>
                         <FormControl
@@ -76,7 +98,7 @@ class AuthForm extends Component {
             </div>
         );
     }
-}
+};
 
 const mapStateToProps = (state) => {
 
@@ -90,6 +112,7 @@ const mapDispatchToProps = (dispatch) => {
     return {
         //signup: ({ username, password }) => dispatch(signup({ username, password })),
         login: ({ username, password }) => dispatch(login({ username, password })),
+        updateMessage: (obj) => dispatch(updateMessage(obj)),
     };
 };
 
