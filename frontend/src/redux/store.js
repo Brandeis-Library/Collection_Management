2b3638bd9c26c3d0053b8f733926f52e6de4aee4
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2023-12-03 20:42:43 -0800

    modified the store to have the redux dev tools avaiable in dev but not prod.

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,17 +1,38 @@
+
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducer";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk, logger),
+let store;
+
+if (process.env.NODE_ENV === 'development') {
+  store = createStore(
+    rootReducer,
     compose(
       applyMiddleware(thunk, logger),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+      compose(
+        applyMiddleware(thunk, logger),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+      ),
     ),
-  ),
-);
+  );
+
+}
+
+
+if (process.env.NODE_ENV === 'production') {
+  store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk, logger),
+      compose(
+        applyMiddleware(thunk, logger),
+
+      ),
+    ),
+  );
+
+}
 
 export default store;
