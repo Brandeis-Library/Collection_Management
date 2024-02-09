3b777f815168b29d3d104e11f9e598eb336c399c
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2024-02-08 17:11:28 -0800

    swap BrowserRouter for HashRouter to fix bugs in the react build links.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root"),
 );
