7e13f9fe77e8fc86f1127df4fcc4afa02eb0befe
HeWhoShallNotBeMentioned <cunderwoodmn@gmail.com>
2024-02-09 19:43:35 -0800

    put back browserrouter into index.js. testing using server ip to replace localhost in AWS server.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root"),
 );
